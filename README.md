f549eb3c23440380af86cc24821b29c402906e0b
Michele <michelemilidoni@gmail.com>
2014-01-27 00:33:44 +0100

    postgres-change-owner first commit

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ system-utility
 ==============
 
 Utility for DBA and SYSADMIN
+
+postgres-change-owner.py
+------------------------
+
+This script changes owner of postgres database. In particular, it changes owner at following db elements:
+
+* database
+* schema
+* tables
+* sequences
+* views
+
+Usage: 
+```
+python postgres-change-owner.py db owner
+```
+
---
+++ postgres-change-owner.py
@@ -0,0 +1,41 @@
+import sys
+
+if sys.version_info < (2, 7):
+    print "Must use python 2.7 or greater"
+    exit(1)
+
+from subprocess import check_output, call
+
+if len(sys.argv) < 3:
+    print "Usage: " + sys.argv[0] + " db " + "owner"
+    exit(1)
+
+db = sys.argv[1]
+owner = sys.argv[2]
+
+tabelle = check_output(["psql -qAt -c \"select tablename from pg_tables where schemaname = 'public';\" " + db], shell=True).strip().split("\n");
+sequenze = check_output(["psql -qAt -c \"select sequence_name from information_schema.sequences where sequence_schema = 'public';\" " + db], shell=True).strip().split("\n");
+viste = check_output(["psql -qAt -c \"select table_name from information_schema.views where table_schema = 'public';\" " + db], shell=True).strip().split("\n");
+
+print "Changing database"
+check_output("psql -c \"ALTER DATABASE " + db + " OWNER TO "+ owner +"\" ", shell=True)
+
+print "Changing schema"
+check_output("psql -c \"ALTER SCHEMA public OWNER TO "+ owner +"\" " + db, shell=True)
+
+print "Changing tables"
+for tbl in tabelle:
+    if tbl:
+        check_output("psql -c \"alter table " + tbl + " owner to " + owner + "\" " + db + ";", shell=True)
+
+print "Changing sequences"
+for tbl in sequenze:
+    if tbl:
+        check_output("psql -c \"alter table " + tbl + " owner to " + owner + "\" " + db + ";", shell=True)
+
+print "Changing views"
+for tbl in viste:
+    if tbl:
+        check_output("psql -c \"alter table " + tbl + " owner to " + owner + "\" " + db + ";", shell=True)
+
+
